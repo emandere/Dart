f5be498fb58367ba23e25b47312bf8b89e52b906
emandere <emandere@gmail.com>
2015-06-25 22:05:31 -0400

    Fixed JSON reference

--- main.dart
+++ main.dart
@@ -1,12 +1,12 @@
 import 'dart:io';
-import 'dart:json' as JSON;
+import 'dart:convert' show JSON;
 
 main() {
   var port = int.parse(Platform.environment['PORT']);
   HttpServer.bind('0.0.0.0', port).then((HttpServer server) {
     print('Server started on port: ${port}');
     server.listen((HttpRequest request) {
-      var resp = JSON.stringify({
+      var resp = JSON.encode({
         'Dart on Heroku': true,
         'Buildpack URL': 'https://github.com/igrigorik/heroku-buildpack-dart',
         'Environment': Platform.environment}
